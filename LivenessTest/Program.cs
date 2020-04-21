824a97fde46d6d23b31263c4de0c4a3ea457351d
Akash Lal <akashl@microsoft.com>
2020-04-22 01:30:10 +0530

    Simple liveness test

---
+++ LivenessTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Coyote.Specifications;
+using Microsoft.Coyote.Tasks;
+
+namespace LivenessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        [Microsoft.Coyote.SystematicTesting.Test]
+        public static async Task Test()
+        {
+            Specification.RegisterMonitor<LivenessMonitor>();
+
+            var lck = AsyncLock.Create();
+            int x = 0;
+
+            var t1 = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    using (await lck.AcquireAsync())
+                    {
+                        x++;
+                    }
+                }
+            });
+
+            var t2 = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    using (await lck.AcquireAsync())
+                    {
+                        x++;
+                    }
+                }
+            });
+
+            await Task.WhenAll(t1, t2);
+        }
+    }
+
+    class LivenessMonitor : Monitor
+    {
+        [Start]
+        [Hot]
+        class S : State { }
+    }
+}
