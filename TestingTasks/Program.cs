b63b469abce7e783cdc1ec76a2a195bbda6d8b87
Akash Lal <akashl@microsoft.com>
2020-09-25 16:44:46 +0530

    Simple app for Tasks

---
+++ TestingTasks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestingTasks
+{
+    class Program
+    {
+        static int x = 0;
+
+        static async Task Bar(int i)
+        {
+            Console.WriteLine("1");
+            await Task.Delay(10);
+            Console.WriteLine("2");
+            await Task.Delay(10);
+            Console.WriteLine("3");
+            await Task.Delay(10);
+            Console.WriteLine("4");
+        }
+
+        static async Task Foo(int i)
+        {
+            await Bar(i);
+        }
+
+        static void Main(string[] args)
+        {
+            var t = new Task<Task>(async () => { await Foo(0); });
+            t.Start();
+            t.Unwrap().Wait();            
+            Console.WriteLine("Done");
+        }
+    }
+}
